be0bbc9c0acc20e4bc09a55be5561a0001a05663
Thien NNT <nguyennhutamthienbk2011@gmail.com>
2015-09-29 09:52:55 +0700

    [Comple] Complete exercises chap 9

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -19,18 +19,21 @@ class UsersEditTest < ActionDispatch::IntegrationTest
 
   test "successful edit with friendly forwarding" do
     get edit_user_path(@user)
-    log_in_as(@user)
-    assert_redirected_to edit_user_path(@user)
-    name  = "Foo Bar"
-    email = "foo@bar.com"
-    patch user_path(@user), user: { name:  name,
-                                    email: email,
-                                    password:              "",
-                                    password_confirmation: "" }
-    assert_not flash.empty?
-    assert_redirected_to @user
-    @user.reload
-    assert_equal name,  @user.name
-    assert_equal email, @user.email
+		assert_not_nil session[:forwarding_url]
+		log_in_as(@user)
+		assert_redirected_to edit_user_path(@user)
+		name = "Foo Bar"
+		email = "foo@bar.com"
+		patch user_path(@user), user: { name: name,
+		email: email,
+		password:
+		"foobar",
+		password_confirmation: "foobar" }
+		assert_not flash.empty?
+		assert_redirected_to @user
+		assert_nil session[:forwarding_url]
+		@user.reload
+		assert_equal @user.name, name
+		assert_equal @user.email, email
   end
 end
